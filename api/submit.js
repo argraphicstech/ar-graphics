06668810a91e22fa35375a04789927bb540f7b1a
argraphicstech <argraphicstech@gmail.com>
2026-05-28 12:58:44 +0530

    Delete api/submit.js

--- api/submit.js
+++
@@ -1,7 +0,0 @@
-import {addCustomer} from "./data";
-export default function handler(req,res){
-if(req.method==="POST"){
-addCustomer(req.body);
-res.status(200).json({msg:"saved"});
-}else res.status(405).end();
-}
